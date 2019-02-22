c832a91cc31e07d5938eebb0ce4cbd70a8e21f46
edko <edko0866@gmail.com>
2019-02-22 14:39:57 +0200

    commit 1

---
+++ src/main/java/model/Family.java
@@ -0,0 +1,19 @@
+package model;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Table
+public class Family {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotBlank(message = "Username cannot be empty")
+    private String name;
+
+    private Long id_parents;
+    private Long id_schoolboy;
+
+}
---
+++ src/main/java/model/Schoolboy.java
@@ -0,0 +1,24 @@
+package model;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Table
+public class Schoolboy {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotBlank(message = "Username cannot be empty")
+    private String login;
+    @NotBlank(message = "Password cannot be empty")
+    private String password;
+
+    private String surname;
+    private String name;
+    private String secondname;
+    private String phone;
+
+    private boolean active;
+}
